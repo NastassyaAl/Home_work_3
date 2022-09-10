006d49e2e97c58d06e55e9c5d5074ffb947aff97
unknown <nastena-alhovik@yandex.ru>
2022-09-10 21:02:50 +0300

    add Task 19 Homework 3

---
+++ Task_19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine();
+string Num = Convert.ToString(number);
+if (number.Length != 5)
+{
+    Console.WriteLine("Неправильное количество символов");
+}
+else
+{
+    if (Num[0] == Num[4] || Num[1] == Num[3])
+    {
+        Console.WriteLine("Это число является полиндромом");
+    }
+    else
+    {
+    Console.WriteLine("Это не полиндром");
+    }
+}
